1ade4dc6eee4ee6365d99c9c76acd45629bc6a89
Kraig Amador <kamador@shopzilla.com>
2011-03-22 17:37:18 +0000

    removing black background

--- src/graph/Plot.java
+++ src/graph/Plot.java
@@ -217,7 +217,7 @@ public final class Plot {
     final PrintWriter gp = new PrintWriter(script_path);
     try {
       // XXX don't hardcode all those settings.  At least not like that.
-      gp.append("set terminal png x000000 xCCCCCC font \"lucida grande\" 9 size ")
+      gp.append("set terminal png size ")
         // Why the fuck didn't they also add methods for numbers?
         .append(Short.toString(width)).append(",")
         .append(Short.toString(height)).append("\n"
